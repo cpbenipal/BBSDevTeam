976c4c32e0bd69dd170938d3df6d2ff09df0b97d
nahom-habtamu <nahomhab2626@gmail.com>
2022-05-05 17:14:28 +0300

    Add Twilio Message Sending Service in SendOTP usecase

--- BBS.Dto/LoginUserOTPDto.cs
+++ BBS.Dto/LoginUserOTPDto.cs
@@ -4,8 +4,10 @@ namespace BBS.Dto
 {
     public class LoginUserOTPDto
     {
-        [Required]
-        public string Email { get; set; } 
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
         [MaxLength(4)]
         public string OTP { get; set; }
     }
